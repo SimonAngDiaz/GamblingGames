46a005175555cef0783e0784e0f43aca481f250f
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2025-05-25 00:39:32 -0400

    button outline

--- button.py
+++ button.py
@@ -1,3 +1,4 @@
+import pygame
 
 class Button():
 	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
@@ -13,10 +14,15 @@ class Button():
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
 		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
+	def draw_outline(self, screen):
+		# Draw a white outline around the button
+		pygame.draw.rect(screen, (255, 255, 255), self.rect, 4)  # Increased thickness to 4
+
 	def update(self, screen):
 		if self.image is not None:
 			screen.blit(self.image, self.rect)
 		screen.blit(self.text, self.text_rect)
+		self.draw_outline(screen)  # Call the method to draw the outline
 
 	def checkForInput(self, position):
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
